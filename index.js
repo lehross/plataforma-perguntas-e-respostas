3eb7aada462c0748779aae513f2eda7614f66bc9
Letícia <leticia.rossaneis@gmail.com>
2020-11-26 15:35:59 -0300

    Bootstrap e arquivos estáticos

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 app.set("view engine", "ejs"); //setup ejs como view engine
+app.use(express.static("public"));
 
 //rotas
 app.get("/", (req, res) => {
