ee6c01a6767c2c4754946ff4652ec06dca567989
Letícia <leticia.rossaneis@gmail.com>
2020-11-30 20:35:04 -0300

    Recebendo dados de formulário de pergunta

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs"); //setup ejs como view engine
 app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 //rotas
 app.get("/", (req, res) => {
@@ -12,6 +16,13 @@ app.get("/", (req, res) => {
 
 app.get("/perguntar", (req, res) => {
     res.render("perguntar");
+});
+
+//receber formulário de pergunta
+app.post("/salvarpergunta", (req, res) => {
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    res.send("Formulário recebido!");
 });
 
 app.listen(3000, () => {
