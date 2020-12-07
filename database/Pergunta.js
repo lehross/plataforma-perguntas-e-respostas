9e6247b4032be1ac27a2b5ef3280115ddb6bff91
Letícia <leticia.rossaneis@gmail.com>
2020-12-07 20:48:37 -0300

    Geração de tabela com model

---
+++ database/Pergunta.js
@@ -0,0 +1,16 @@
+const Sequelize = require("sequelize");
+const connection = require("./database");
+
+//criar um model
+const Pergunta = connection.define('pergunta', { //nome da tabela
+    titulo: {
+        type: Sequelize.STRING, //textos curtos
+        allowNull: false
+    },
+    descricao: {
+        type: Sequelize.TEXT, //textos longos
+        allowNull: false
+    }
+});
+
+Pergunta.sync({force: false}).then(() => {}); //sincronizar com o banco de dados
--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 const bodyParser = require("body-parser");
 const connection = require("./database/database");
 
+const perguntaModel = require("./database/Pergunta");
+
 //database
 connection
     .authenticate()
