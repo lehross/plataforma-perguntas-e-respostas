7b3b973711af3f20055353d9e0742c651e607c44
Letícia <leticia.rossaneis@gmail.com>
2020-12-10 21:05:59 -0300

    Exibir respostas

--- index.js
+++ index.js
@@ -60,8 +60,14 @@ app.get("/pergunta/:id", (req, res) => {
         where: {id: id}
     }).then(pergunta => {
         if(pergunta != undefined) {
-            res.render("pergunta", {
-                pergunta: pergunta //passar a pergunta para o front
+            Resposta.findAll({
+                where: {perguntaId: pergunta.id},
+                order: [['id', 'DESC']]
+            }).then(respostas => {
+                res.render("pergunta", {
+                    pergunta: pergunta, //passar a pergunta para o front
+                    respostas: respostas
+                });
             });
         } else {
             res.redirect("/");
